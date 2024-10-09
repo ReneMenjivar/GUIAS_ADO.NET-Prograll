86142a691e63d8c5c391bb03df266851e6176691
René Lemus <126819110+ReneMenjivar@users.noreply.github.com>
2024-10-09 12:24:31 -0600

    Add files via upload
    Nuevos Cambios

---
+++ AccesoDatos/CustomerRepository.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class CustomerRepository
+    {
+        public DataTable ObtenerTodos()
+        {
+            DataTable dataTable = new DataTable();
+
+            String select = "";
+            select = select + "SELECT [CustomerID] " + "\n";
+            select = select + "      ,[CompanyName] " + "\n";
+            select = select + "      ,[ContactName] " + "\n";
+            select = select + "      ,[ContactTitle] " + "\n";
+            select = select + "      ,[Address] " + "\n";
+            select = select + "      ,[City] " + "\n";
+            select = select + "      ,[Region] " + "\n";
+            select = select + "      ,[PostalCode] " + "\n";
+            select = select + "      ,[Country] " + "\n";
+            select = select + "      ,[Phone] " + "\n";
+            select = select + "      ,[Fax] " + "\n";
+            select = select + "  FROM [dbo].[Customers]";
+
+            var conexion = @"Data Source=.\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;";
+            SqlDataAdapter adapter = new SqlDataAdapter(select, conexion);
+            adapter.Fill(dataTable);
+
+            return dataTable;
+        }
+
+        public Customers ObtenerPorID(string id)
+        {
+            using (var conexion = DataBase.GetSqlConnection())
+            {
+                var dataTable = new DataTable();
+
+                String selectForId = "";
+                selectForId = selectForId + "SELECT [CustomerID] " + "\n";
+                selectForId = selectForId + "      ,[CompanyName] " + "\n";
+                selectForId = selectForId + "      ,[ContactName] " + "\n";
+                selectForId = selectForId + "      ,[ContactTitle] " + "\n";
+                selectForId = selectForId + "      ,[Address] " + "\n";
+                selectForId = selectForId + "      ,[City] " + "\n";
+                selectForId = selectForId + "      ,[Region] " + "\n";
+                selectForId = selectForId + "      ,[PostalCode] " + "\n";
+                selectForId = selectForId + "      ,[Country] " + "\n";
+                selectForId = selectForId + "      ,[Phone] " + "\n";
+                selectForId = selectForId + "      ,[Fax] " + "\n";
+                selectForId = selectForId + "  FROM [dbo].[Customers] " + "\n";
+                selectForId = selectForId + "  Where CustomerID = @CustomerID";
+               
+                using (var comando = new SqlCommand(selectForId, conexion))
+                {
+                    comando.Parameters.AddWithValue("@CustomerID", id);
+                    SqlDataAdapter adaptador = new SqlDataAdapter(comando);
+                    adaptador.Fill(dataTable);
+                    Customers cliente = ExtraerInformacionCliente(dataTable);
+                    return cliente;
+                }
+            }
+        }
+
+        public Customers ExtraerInformacionCliente(DataTable dataTable)
+        {
+            Customers customer = new Customers();
+            foreach (DataRow fila in dataTable.Rows)
+            {
+                customer.CustomerID = fila.Field<string>("CustomerID");
+                customer.CompanyName = fila.Field<string>("CompanyName");
+                customer.ContactName = fila.Field<string>("ContactName");
+                customer.ContactTitle = fila.Field<string>("ContactTitle");
+                customer.Address = fila.Field<string>("Address");
+                customer.City = fila.Field<string>("City");
+                customer.Region = fila.Field<string>("Region");
+                customer.PostalCode = fila.Field<string>("PostalCode");
+                customer.Country = fila.Field<string>("Country");
+                customer.Phone = fila.Field<string>("Phone");
+                customer.Fax = fila.Field<string>("Fax");
+            }
+            return customer;
+        }
+
+        public int InsertarCliente(Customers cliente)
+        {
+            using (var conexion = DataBase.GetSqlConnection())
+            {
+                String InsertarporId = "";
+                InsertarporId = InsertarporId + "INSERT INTO [dbo].[Customers] " + "\n";
+                InsertarporId = InsertarporId + "           ([CustomerID] " + "\n";
+                InsertarporId = InsertarporId + "           ,[CompanyName] " + "\n";
+                InsertarporId = InsertarporId + "           ,[ContactName] " + "\n";
+                InsertarporId = InsertarporId + "           ,[ContactTitle] " + "\n";
+                InsertarporId = InsertarporId + "           ,[Address]) " + "\n";
+                InsertarporId = InsertarporId + "     VALUES " + "\n";
+                InsertarporId = InsertarporId + "           (@CustomerID " + "\n";
+                InsertarporId = InsertarporId + "           ,@CompanyName " + "\n";
+                InsertarporId = InsertarporId + "           ,@ContactName " + "\n";
+                InsertarporId = InsertarporId + "           ,@ContactTitle " + "\n";
+                InsertarporId = InsertarporId + "           ,@Address)";
+
+                using (var commando = new SqlCommand(InsertarporId, conexion))
+                {
+                    SqlCommand comando = parametrosSqlCustomers(commando, cliente);
+                    SqlDataAdapter adaptador = new SqlDataAdapter(comando);
+                    adaptador.InsertCommand = comando;
+                    return adaptador.InsertCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private SqlCommand parametrosSqlCustomers(SqlCommand comando, Customers cliente)
+        {
+            comando.Parameters.AddWithValue("CustomerID", cliente.CustomerID);
+            comando.Parameters.AddWithValue("CompanyName", cliente.CompanyName);
+            comando.Parameters.AddWithValue("ContactName", cliente.ContactName);
+            comando.Parameters.AddWithValue("ContactTitle", cliente.ContactTitle);
+            comando.Parameters.AddWithValue("Address", cliente.Address);
+            return comando;
+        }
+
+        public int ActualizarCliente(Customers cliente)
+        {
+            using (var conexion = DataBase.GetSqlConnection())
+            {
+                String updateUser = "";
+                updateUser = updateUser + "UPDATE [dbo].[Customers] " + "\n";
+                updateUser = updateUser + "   SET [CustomerID] = @CustomerID " + "\n";
+                updateUser = updateUser + "      ,[CompanyName] = @CompanyName " + "\n";
+                updateUser = updateUser + "      ,[ContactName] = @ContactName " + "\n";
+                updateUser = updateUser + "      ,[ContactTitle] = @ContactTitle " + "\n";
+                updateUser = updateUser + "      ,[Address] = @Address " + "\n";
+                updateUser = updateUser + " WHERE CustomerID = @CustomerID";
+
+                using (var commando = new SqlCommand(updateUser, conexion))
+                {
+                    SqlCommand comando = parametrosSqlCustomers(commando, cliente);
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    adapter.UpdateCommand = comando;
+                    return adapter.UpdateCommand.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
